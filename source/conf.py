dcb98ef28cb26386eee8bf2dedad3e05a49e9da8
Brian King <brianjosephking@gmail.com>
2016-01-28 22:13:19 -0600

    attempting to use paymentfont

--- source/conf.py
+++ source/conf.py
@@ -303,6 +303,12 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
+# PaymentFont Support
+
+def setup(app):
+    app.add_stylesheet('css/paymentfont.css')
+
+
 # Markdown support
 
 from recommonmark.parser import CommonMarkParser
